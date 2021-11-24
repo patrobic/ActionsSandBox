033442d88abb9d9c60544e12994d1cf0dbf58fa0
Patrick Robichaud <patrick.robichaud@tecsys.com>
2021-11-23 20:49:05 -0500

    Added Divide/Subtract, more tests.

--- ActionsSandbox.Library/Class1.cs
+++ ActionsSandbox.Library/Class1.cs
@@ -7,9 +7,20 @@
             return a + b;
         }
 
+        public int Subtract(int left, int right)
+        {
+            return left - right;
+        }
+
         public int Multiply(int a, int b)
         {
             return (a * b);
         }
+
+        public double Divide(int num, int den)
+        {
+            return num / (double)den;
+        }
+
     }
 }
--- ActionsSandbox.Test/UnitTest1.cs
+++ ActionsSandbox.Test/UnitTest1.cs
@@ -12,5 +12,21 @@ namespace ActionsSandbox.Test
             var result = math.Add(1, 2);
             Assert.Equal(3, result);
         }
+
+        [Fact]
+        public void TestSubtract()
+        {
+            var math = new Class1();
+            var result = math.Subtract(5, 3);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void TestMultiply()
+        {
+            var math = new Class1();
+            var result = math.Multiply(2, 3);
+            Assert.Equal(6, result);
+        }
     }
 }
