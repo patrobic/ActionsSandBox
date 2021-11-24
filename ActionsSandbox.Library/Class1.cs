42d5259d83170d095b1d037cd8df0c18be9f32cf
Patrick Robichaud <patrick.robichaud@tecsys.com>
2021-11-23 20:32:26 -0500

    Added missing projects.

---
+++ ActionsSandbox.Library/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace ActionsSandbox.Library
+{
+    public class Class1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return (a * b);
+        }
+    }
+}
