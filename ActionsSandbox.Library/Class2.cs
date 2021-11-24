5304d9d6743017a44f819d9f973725beae6472bb
Patrick Robichaud <patrick.robichaud@tecsys.com>
2021-11-23 21:10:26 -0500

    Added 2nd test.

---
+++ ActionsSandbox.Library/Class2.cs
@@ -0,0 +1,10 @@
+﻿namespace ActionsSandbox.Library
+{
+    public class Class2
+    {
+        public int CountItems<T>(IList<T> items)
+        {
+            return items.Count;
+        }
+    }
+}
