8bddde435a316521fe52d059272682e5359afb78
Patrick Robichaud <patrick.robichaud@tecsys.com>
2021-11-23 21:10:26 -0500

    Added 2nd test.

---
+++ ActionsSandbox.Test/UnitTest2.cs
@@ -0,0 +1,17 @@
+﻿using ActionsSandbox.Library;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ActionsSandbox.Test
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            var math = new Class2();
+            var result = math.CountItems(new List<int> { 1, 2, 3 });
+            Assert.Equal(3, result);
+        }
+    }
+}
